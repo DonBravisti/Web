66a43a7c3d63ba64ceb7773d966d7c2875f6f7be
DonBravisti <butov.s-butov@yandex.ru>
2022-12-27 20:24:30 +0300

    Добавил меню-бургер

---
+++ menuBurger.js
@@ -0,0 +1,8 @@
+let header = document.querySelector('.header');
+let burger = document.querySelector('.menu_burger')
+let burgerMenu = document.querySelector('.open_menu')
+burger.onclick = function () {
+    burgerMenu.classList.toggle('to_hide')
+}
+
+console.log('hey');
